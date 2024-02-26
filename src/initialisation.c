f1ea0d9c5a4e262e191d30bdf7a75407529e695c
SavioCAT <savio.boissinot@edu.esiee.fr>
2024-02-26 11:49:35 +0100

    continuité dev initialisation.c

--- src/initialisation.c
+++ src/initialisation.c
@@ -22,3 +22,21 @@ int main() {
 
     return 0;
 }
+
+int open_tube_client_to_server() {
+    int pipe0 = open(*Client_ServeurDeRoutage, O_RDWR);
+    if (pipe0 == -1) {
+        printf("Erreur lors de l'ouverture du pipe Client vers Serveur De Routage\n");
+        return -1;
+    }
+    return pipe0;
+}
+
+int open_tube_server_to_client() {
+    int pipe1 = open(*ServeurDeRoutage_Client, O_RDWR);
+    if (pipe1 == -1) {
+        printf("Erreur lors de l'ouverture du pipe Serveur de Routage vers Client\n");
+        return -1;
+    }
+    return pipe1;
+}
