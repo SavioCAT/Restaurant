a6e50f7ba1ec21f754ac427c616b43a0392b6fe9
SavioCAT <savio.boissinot@edu.esiee.fr>
2024-02-26 11:05:27 +0100

    Ecriture initialisation

--- src/initialisation.c
+++ src/initialisation.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main() {
-
+    int pipe0 = mkfifo(*Client_ServeurDeRoutage, 0777);
+    int pipe1 = mkfifo(*ServeurDeRoutage_Client, 0777);
 
     return 0;
 }
