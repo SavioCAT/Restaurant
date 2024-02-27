d509e5c6dbe395339bfa4e6fb4b181ce49db3a76
SavioCAT <savio.boissinot@edu.esiee.fr>
2024-02-27 01:12:17 +0100

    Debug

--- src/Pipe_cross.c
+++ src/Pipe_cross.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 int open_pipe (char* NamePipe) {
     int pipe = open(NamePipe, O_RDWR); //Opening the pipe in read and write
-    if (pipe == -1) { //Check if the pipe has been opened, if not return -1 and display an error
-        printf("Error while opening the pipe: %s\n", NamePipe);
+    printf("Pipe opened: %s \n", NamePipe);
+    if (pipe == -1) {
+        printf("Error: Error while opening the pipe: %s\n", NamePipe);
         return -1;
     }
-    printf("Pipe opened: %s\n", NamePipe);
-    return pipe; //Send 0 if the pipe has been opened
+    return pipe;
+}
+
+int write_pipe (char* NamePipe, char* Text) {
+    char* buffer = (char*)malloc(sizeof(Text) + 1); //Allocating memory for the buffer
+    strcpy(buffer, Text); //Copying the text to the buffer
+    int write_pipe_acces = open_pipe(NamePipe); //Opening the pipe
+    int writing = (int)write(write_pipe_acces, buffer, sizeof(Text));
+
+    if (writing == -1) {
+        printf("Error: Error while writing to the pipe: %s\n", NamePipe);
+        return -1;
+    }
+    printf("Data written to the pipe: %s\n", buffer);
+    return writing;
 }
 
 int main(void) {
-    int pipe_client_to_server = open_pipe("./File_pipe/pipe_Client_to_Server");
-    int pipe_server_to_client = open_pipe("./File_pipe/pipe_Server_to_Client");
+    int write = write_pipe("../File_pipe/pipe_Client_to_Server", "Hello, World!");
 
     return 0;
 }
