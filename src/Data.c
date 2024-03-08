2ea41d4c1c2283c943120f0a41fc3286cba70485
SavioCAT <savio.boissinot@edu.esiee.fr>
2024-03-08 11:59:28 +0100

    fermeture du pipe en fin de lecture

--- src/Data.c
+++ src/Data.c
@@ -3,6 +3,7 @@
 #include "../header/Pipe_cross.h"
 
 int read_txt_doc(char* name) {
+    int pipe_Data_to_routing = open_pipe("../File_pipe/pipe_Data_to_Routing");
     FILE *f;
     f = fopen(name, "r");
 
@@ -11,5 +12,7 @@ int read_txt_doc(char* name) {
         return 0;
     }
 
-
+    fclose(f);
+    int closing_the_pipe = close_pipe(pipe_Data_to_routing);
+    return closing_the_pipe;
 }
