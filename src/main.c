a38f441cbea4d5d9917a56ce371f30ea45c0110e
SavioCAT <savio.boissinot@edu.esiee.fr>
2024-03-12 20:43:34 +0100

    lol

--- src/main.c
+++ src/main.c
@@ -5,16 +5,6 @@
 #include "../header/Client.h"
 #include "../header/Data.h"
 
-#define BUFFER_SIZE 256
-
 int main() {
-    write_pipe("../File_pipe/pipe_Client_to_Routing", "1");
-
-    int pipe0 = open_pipe("../File_pipe/pipe_Client_to_Routing");
-    char* text = (char*) malloc(32);
-    read_pipe(pipe0, text);
-    close_pipe(pipe0);
-    printf("%s", text);
-    free(text);
-    exit(0);
+    ini_interface();
 }
