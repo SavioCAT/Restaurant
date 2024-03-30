877fc80f220caa833b6c83f3a0800071fec7a1a3
SavioCAT <savio.boissinot@edu.esiee.fr>
2024-03-30 11:35:25 +0100

    ajout de l'identification des pipe avec le pid

--- src/main.c
+++ src/main.c
@@ -2,19 +2,33 @@
 #include "../header/data.h"
 #include "../header/routing.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 4096
 
 int main() {
-    Pipe* client_pipe = malloc(sizeof(char) * 64);
-    Pipe* server_pipe = malloc(sizeof(char) * 64);
+    Pipe* client_pipe = malloc(sizeof(Pipe));
+    Pipe* server_pipe = malloc(sizeof(Pipe));
+    pid_t pid_process = getpid();
+    char pid_string[BUFFER_SIZE];
+    snprintf(pid_string, sizeof(pid_string), "%d", pid_process);
 
-    create_pipe("pipe_client_right", "pipe_client_left");
-    initialise_pipe(client_pipe, "pipe_client_right", "pipe_client_left");
-    create_pipe("pipe_server_right", "pipe_server_left");
-    initialise_pipe(server_pipe, "pipe_server_right", "pipe_server_left");
+    char name_pipe_client_right[BUFFER_SIZE + 256];
+    char name_pipe_client_left[BUFFER_SIZE + 256];
+    char name_pipe_server_right[BUFFER_SIZE + 256];
+    char name_pipe_server_left[BUFFER_SIZE + 256];
+
+    snprintf(name_pipe_client_right, sizeof(name_pipe_client_right), "pipe_client_right%s", pid_string);
+    snprintf(name_pipe_client_left, sizeof(name_pipe_client_left), "pipe_client_left%s", pid_string);
+    snprintf(name_pipe_server_right, sizeof(name_pipe_server_right), "pipe_server_right%s", pid_string);
+    snprintf(name_pipe_server_left, sizeof(name_pipe_server_left), "pipe_server_left%s", pid_string);
+
+    create_pipe(name_pipe_client_right, name_pipe_client_left);
+    initialise_pipe(client_pipe, name_pipe_client_right, name_pipe_client_left);
+    create_pipe(name_pipe_server_right, name_pipe_server_left);
+    initialise_pipe(server_pipe, name_pipe_server_right, name_pipe_server_left);
 
     ini_client(client_pipe);
     ini_routing(client_pipe, server_pipe);
